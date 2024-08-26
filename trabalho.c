d6e4e95d90f26a45abed68cc63ec7ccc6cbecd18
Diehlgit <arthurdiehlbarroso@gmail.com>
2024-08-26 20:01:13 -0300

    Logica aparentemente funciona (TODO: Limpar o código)

--- trabalho.c
+++ trabalho.c
@@ -24,8 +24,9 @@ pthread_cond_t jgr;                 /* Variável de condição para fazer o joga
 
 
 int nCampo;                         /* Nº de jogadores em campo */
-int newMsg = 0;                         /* (0 ou 1) Determina se há uma nova mensagem */
-int ack = 0;                            /* (0 ou 1) Determina se o treinador recebeu a mensagem */
+int newMsg = 1;                         /* (0 ou 1) Determina se há uma nova mensagem */
+int ack = 1;                            /* (0 ou 1) Determina se o treinador recebeu a mensagem */
+int jgrBanco = 1;                       /* (0 ou 1) Determina se o jogador do banco deve esperar */
 
 void treinador() {
     int jogador;
@@ -35,7 +36,8 @@ void treinador() {
     while(1){
         /* Vê se há uma nova mensagem */
         pthread_mutex_lock(&msg);
-        while(newMsg == 0) {
+        while(newMsg) {
+            printf("Treinador: Vou esperar um jogador ficar cansado.\n");
             pthread_cond_wait(&trn, &msg);
         }
          
@@ -44,15 +46,21 @@ void treinador() {
         jogador = mensagem.jogador;
         energia = mensagem.energia;
 
-        newMsg == 0;    
+        newMsg = 1;    
         pthread_mutex_unlock(&msg);
+                    
+        if(energia <= ENERGIA/5) {
+            printf("Treinador: Vou tirar um jogador do banco.\n");
 
-        if(energia < ENERGIA/5) {
-            
-            
+            pthread_mutex_lock(&msgBanco);
 
+            jgrBanco = 0;
+            pthread_cond_signal(&banco);
+
+            pthread_mutex_unlock(&msgBanco);
+        
         }
-
+        
     }
 }
 
@@ -62,7 +70,7 @@ void* jogador(void *arg) {
     int energia = ENERGIA;
     /* Jogadores entram em campo */
     pthread_mutex_lock(&cmp);
-        if (nCampo < 11) {
+        if (nCampo < NJ) {
             nCampo ++;
             jogando = 1;
             printf("Jogador %d no campo. \n", id);
@@ -77,50 +85,50 @@ void* jogador(void *arg) {
 
         /* Se o jogador está em campo, vai jogar, caso contrário descansa */    
         if(jogando) {
-            energia -= rand()%6 + 5;
-            sleep(1);
+            energia -= rand()%21;
+            sleep(2);
         /* Se a ernegia está ficando baixa avisa o treinador */
 
             if(energia < ENERGIA/5) {
-                printf("Jogador %d minha energia está baixa: %d. \n", id, energia);
+                printf("Jogador %d: Minha energia está baixa: %d. \n", id, energia);
                 pthread_mutex_lock(&msg);           /* É possível implementar com try lock para os jogadores não pararem */
                 mensagem.jogador = id;
                 mensagem.energia = energia;
                 
-                newMsg = 1;
+                newMsg = 0;
                 pthread_cond_signal(&trn);                
                 pthread_mutex_unlock(&msg);
 
-                pthread_mutex_lock(&ans);
-                while(ack == 0) {
-                    pthread_cond_wait(&jgr, &ans);      /* Espera a resposta do treinador */
-                }
+                printf("Jogador %d: Entrando no banco.\n", id);
 
-                ack = 0;
-                phread_mutex_unlock(&ans);
-        
+                jogando = 0;
+
             }
 
         } else {
         /* Jogadores no banco descansam */
             
-            
-            
-            if(energia <= ENERGIA-5) {
+            if(energia <= ENERGIA-25) {
                 energia += rand()%21 + 5;
-                printf("%d Recuperando energia: %d \n", id, energia);
+                sleep(2);
             } else {
-                printf("%d Estou descansado. Vou esperar o treinador me chamar. \n", id);
+                printf("Jogador %d: Estou descansado: %d. Vou esperar o treinador me chamar. \n", id, energia);
                 pthread_mutex_lock(&msgBanco);
-                while(jgrBanco == 0) {
+                
+                while(jgrBanco) {
 
-                    pthread_cond_wait(&banco);
+                    pthread_cond_wait(&banco, &msgBanco);
 
                 }
 
+                jgrBanco = 1;
+
                 pthread_mutex_unlock(&msgBanco);
+
+                printf("Jogador %d: Entrando em campo.\n", id);
+
+                jogando = 1;
             }
-            sleep(5);
         }
 
     }
